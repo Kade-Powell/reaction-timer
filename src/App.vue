f5a6f20fe99c07477321b532a098314077adff41
Powell <Kade.Powell@cox.com>
2022-05-03 14:19:52 -0400

    almost done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <ClickBlock v-if="isPlaying" :delay="delay" @end="endGame" />
+  <ClickResults v-if="score !== null" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ClickBlock from "./components/ClickBlock.vue";
+import ClickResults from "./components/ClickResults.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ClickBlock, ClickResults },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.score = null;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+    },
+  },
+};
 </script>
 
 <style>
